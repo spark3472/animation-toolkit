0efbcbf28f94f15ee8bbc84f5c75f73f2c4d8dc0
Ashley Park <55250326+spark3472@users.noreply.github.com>
2021-09-09 14:56:06 -0400

    Update sphere3d.cpp

--- assignments/a0-start/sphere3d.cpp
+++ assignments/a0-start/sphere3d.cpp
@@ -1,20 +1,40 @@
 #include "atkui/framework.h"
 
 using glm::vec3;
+using namespace std;
 
 class Sphere3D : public atkui::Framework {
  public:
   Sphere3D() : atkui::Framework(atkui::Perspective) {
   }
+  virtual void setup(){
+    currentPos = vec3(0);
+    vel = vec3(0);
+  }
+  virtual void scene(){
+    // update
+    currentPos = currentPos + 50.0f * vel * dt();
 
-  virtual void scene() {
     // colors are RGB triplets in range [0,1]
     setColor(vec3(0,1,0));
-
     // draw a sphere at center of the world
     float radius = 50.0;
-    drawSphere(vec3(0), radius);
+    drawSphere(currentPos, radius);
+
   }
+
+  void keyUp(int key, int mods) {
+    if (key == GLFW_KEY_SPACE) {
+      vel = agl::randomUnitVector();
+      //currentPos = currentPos + vel * dt();
+    }else if (key == GLFW_KEY_R) {
+      currentPos = vec3(0);
+      vel = vec3(0);
+    }
+  }
+  vec3 currentPos;
+  vec3 vel;
+  int key;
 };
 
 int main(int argc, char** argv)
