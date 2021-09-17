b4f01eb69b4525116e6657a590e9b64dbd7f12ad
Ashley Park <55250326+spark3472@users.noreply.github.com>
2021-09-17 15:53:39 -0400

    Update look.cpp

--- assignments/a1-hello/look.cpp
+++ assignments/a1-hello/look.cpp
@@ -6,26 +6,54 @@ class Look : public atkui::Framework {
  public:
   Look() : atkui::Framework(atkui::Orthographic) {
   }
-
+  float lwidth = width()*0.375;
+  float rwidth = width()*0.625;
   virtual void setup() {
+    radius = 100;
+    left = vec3(lwidth, height() * 0.5, 0);
+    right = vec3(rwidth, height() * 0.5, 0);
+    
+    leftCenter = vec3(lwidth, height() * 0.5, 0);
+    rightCenter = vec3(rwidth, height() * 0.5, 0);
+    vel = vec3(0.65, 0, 0);
     _mouseX = width() * 0.5;
     _mouseY = height() * 0.5;
   }
 
   virtual void scene() {
     vec3 target = vec3(_mouseX, _mouseY, 0);
-    setColor(vec3(1,0,0));
+    setColor(vec3(1, 0, 0));
     drawSphere(target, 5);
-  }
+    setColor(vec3(1));
+    drawSphere(right, radius);
+    drawSphere(left,  radius);
+    
+    leftPupil = left + vec3(25 * cos(atan2(_mouseY - lwidth, _mouseX-height()*0.5)), 25 * sin(atan2(_mouseY-lwidth, _mouseX-height()*0.5)), 100);
+    rightPupil = right + vec3(25 * cos(atan2(_mouseY - rwidth, _mouseX-height()*0.5)), 25 * sin(atan2(_mouseY-rwidth, _mouseX-height()*0.5)), 100);
+    setColor(vec3(0));
+    drawSphere(rightPupil, 30);
+    drawSphere(leftPupil, 30); 
 
-  void mouseMove(int x, int y) {
-    _mouseX = x;
-    _mouseY = height() - y;
+  }
+  
+  void mouseMotion(int x, int y, int dx, int dy) {
+    if (mouseIsDown(GLFW_MOUSE_BUTTON_LEFT)){
+      _mouseX = x;
+      _mouseY = height() - y;
+    }
   }
 
  private:
   int _mouseX;
   int _mouseY;
+  vec3 right;
+  vec3 left;
+  vec3 rightPupil;
+  vec3 leftPupil;
+  vec3 vel;
+  vec3 rightCenter;
+  vec3 leftCenter;
+  float radius;
 };
 
 int main(int argc, char** argv) {
