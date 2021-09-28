ce32f9511d14c803de1db6626f3c1d7778edc0e9
Ashley Park <apark2@brynmawr.edu>
2021-09-28 14:00:06 -0400

    helpful message

--- assignments/a1-hello/look.cpp
+++ assignments/a1-hello/look.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include "atkui/framework.h"
 
 using namespace glm;
@@ -62,40 +62,4 @@ int main(int argc, char** argv) {
   viewer.run();
   return 0;
 }
-=======
-#include "atkui/framework.h"
 
-using namespace glm;
-
-class Look : public atkui::Framework {
- public:
-  Look() : atkui::Framework(atkui::Orthographic) {
-  }
-
-  virtual void setup() {
-    _mouseX = width() * 0.5;
-    _mouseY = height() * 0.5;
-  }
-
-  virtual void scene() {
-    vec3 target = vec3(_mouseX, _mouseY, 0);
-    setColor(vec3(1,0,0));
-    drawSphere(target, 5);
-  }
-
-  void mouseMove(int x, int y) {
-    _mouseX = x;
-    _mouseY = height() - y;
-  }
-
- private:
-  int _mouseX;
-  int _mouseY;
-};
-
-int main(int argc, char** argv) {
-  Look viewer;
-  viewer.run();
-  return 0;
-}
->>>>>>> upstream/main
--- assignments/a1-hello/unique.cpp
+++ assignments/a1-hello/unique.cpp
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #include "atkui/framework.h"
 
 using namespace glm;
@@ -80,29 +80,4 @@ int main(int argc, char** argv)
    viewer.run();
    return 0;
 }
-=======
-#include "atkui/framework.h"
 
-using namespace glm;
-
-class Unique : public atkui::Framework {
- public:
-  Unique() : atkui::Framework(atkui::Orthographic) {
-  }
-
-  virtual void setup() {
-    // your code here!
-  }
-
-  virtual void scene() {
-    // your code here!
-  }
-};
-
-int main(int argc, char** argv) {
-  Unique viewer;
-  viewer.run();
-  return 0;
-}
-
->>>>>>> upstream/main
