9abc65a1f85ec9f6096bdcd17dbc0aa3e98016ff
Ashley Park <apark2@brynmawr.edu>
2021-09-28 21:38:28 -0400

    helpful message

--- assignments/a3-splines/interpolator_catmullrom.h
+++ assignments/a3-splines/interpolator_catmullrom.h
@@ -15,12 +15,33 @@ public:
     InterpolatorCatmullRom() : Interpolator("Catmull-Rom") {}
 
     virtual glm::vec3 interpolate(int segment, float u) const {
-       // todo: your code here
-       // see: interpolator.h and writeup for details
-       return glm::vec3(0);
+       if (segment == 0){
+         return getControlPoint(segment)*(1.0f-u)*(1.0f-u)*(1.0f-u) + 3.0f*u*(1.0f-u)*(1.0f-u)*getControlPoint(segment + 3) + 3.0f*u*u*(1.0f-u)*getControlPoint(segment + 4) + u*u*u*getControlPoint(segment + 1); 
+       }else{//else segment equals 1
+         return getControlPoint(segment)*(1.0f-u)*(1.0f-u)*(1.0f-u) + 3.0f*u*(1.0f-u)*(1.0f-u)*getControlPoint(segment + 4) + 3.0f*u*u*(1.0f-u)*getControlPoint(segment + 5) + u*u*u*getControlPoint(segment + 1); 
+       }
     }
 
     virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
+       float fraction = ((float)1/(float)6);
+       mCtrlPoints = keys;
+       glm::vec3 b00, b01, b02, b03;
+       glm::vec3 b10, b11, b12, b13;
+       b00 = mCtrlPoints[0];
+       b03 = mCtrlPoints[1];
+       b10 = mCtrlPoints[1];
+       b13 = mCtrlPoints[2];
+
+       b01 = b00 + fraction*(b03-b00);
+       b02 = b03 - fraction*(b13-b00);
+       b11 = b10 + fraction*(b13-b00);
+       b12 = b13 - fraction*(b13-b10);
+
+       mCtrlPoints.push_back(b01);
+       mCtrlPoints.push_back(b02);
+       mCtrlPoints.push_back(b11);
+       mCtrlPoints.push_back(b12);
+       
        // todo: your code here
        // see: interpolator.h and writeup for details
     }
--- assignments/a3-splines/interpolator_hermite.h
+++ assignments/a3-splines/interpolator_hermite.h
@@ -6,6 +6,9 @@
 #include <Eigen/Dense>
 #include "interpolator.h"
 
+using namespace Eigen;
+using namespace std;
+
 //--------------------------------
 // Hermite 
 //--------------------------------
@@ -16,11 +19,72 @@ public:
     InterpolatorHermite() : Interpolator("Hermite"), mClampDir(glm::vec3(1,0,0)) {}
 
     virtual glm::vec3 interpolate(int segment, float u) const {
+
        // todo: your code here
        return glm::vec3(0);
     }
 
     virtual void computeControlPoints(const std::vector<glm::vec3>& keys) {
+
+        mCtrlPoints = keys;
+        mCtrlPoints[1];
+        MatrixXf A(5, 5);
+        A(0, 0) = 2;
+        A(1, 0) = 1;
+        A(2, 0) = 0;
+        A(3, 0) = 0;
+        A(4, 0) = 0;
+        A(0, 1) = 1;
+        A(1, 1) = 4;
+        A(2, 1) = 1;
+        A(3, 1) = 0;
+        A(4, 1) = 0;
+        A(0, 2) = 0;
+        A(1, 2) = 1;
+        A(2, 2) = 4;
+        A(3, 2) = 1;
+        A(4, 2) = 0;
+        A(0, 3) = 0;
+        A(1, 3) = 0;
+        A(2, 3) = 1;
+        A(3, 3) = 4;
+        A(4, 3) = 1;
+        A(0, 4) = 0;
+        A(1, 4) = 0;
+        A(2, 4) = 0;
+        A(3, 4) = 1;
+        A(4, 4) = 2;
+
+        if (isClamped()){
+            glm::vec3 v0, v1, v2, v3;
+            v0 = getClampDirection();
+            
+            MatrixXf b(5, 3);
+            b(0, 0) = v0.x;
+            b(0, 1) = v0.y;
+            b(0, 2) = v0.z;
+            b(1, 0) = (3.0f*(mCtrlPoints[2] - mCtrlPoints[0]).x);
+            b(1, 1) = (3.0f*(mCtrlPoints[2] - mCtrlPoints[0]).y);
+            b(1, 2) = (3.0f*(mCtrlPoints[2] - mCtrlPoints[0]).z);
+            b(2, 0) = (3.0f*(mCtrlPoints[3] - mCtrlPoints[1]).x);
+            b(2, 1) = (3.0f*(mCtrlPoints[3] - mCtrlPoints[1]).y);
+            b(2, 2) = (3.0f*(mCtrlPoints[3] - mCtrlPoints[1]).z);
+            b(3, 0) = (3.0f*(mCtrlPoints[4] - mCtrlPoints[2]).x);
+            b(3, 1) = (3.0f*(mCtrlPoints[4] - mCtrlPoints[2]).y);
+            b(3, 2) = (3.0f*(mCtrlPoints[4] - mCtrlPoints[2]).z);
+            b(4, 0) = v0.x;
+            b(4, 1) = v0.y;
+            b(4, 2) = v0.z;
+
+            MatrixXf x = A.llt().solve(b);
+            cout << A << endl;
+            cout << b << endl;
+            cout << x << endl;
+            
+            
+
+       }
+        
        // todo: your code here
     }
 
