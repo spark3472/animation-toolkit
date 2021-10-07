7de96adc90e6eaeb51504ae9c4a9bf7d3436bf0c
Ashley Park <55250326+spark3472@users.noreply.github.com>
2021-10-07 16:37:51 -0400

    Update matrix3-basecode.cpp

--- libsrc/atkmath/matrix3-basecode.cpp
+++ libsrc/atkmath/matrix3-basecode.cpp
@@ -1,36 +1,85 @@
 #include "atkmath/matrix3.h"
 #include "atkmath/quaternion.h"
+//https://www.symbolab.com/solver/step-by-step/%5Cbegin%7Bpmatrix%7D1%260%260%5C%5C%200%26cos%5Cleft(x%5Cright)%26-sin%5Cleft(x%5Cright)%5C%5C%200%26sin%5Cleft(x%5Cright)%26cos%5Cleft(x%5Cright)%5Cend%7Bpmatrix%7D%5Ccdot%5Cbegin%7Bpmatrix%7Dcos%5Cleft(y%5Cright)%260%26sin%5Cleft(y%5Cright)%5C%5C%200%261%260%5C%5C%20-sin%5Cleft(y%5Cright)%260%26cos%5Cleft(y%5Cright)%5Cend%7Bpmatrix%7D%5Ccdot%5Cbegin%7Bpmatrix%7Dcos%5Cleft(z%5Cright)%26-sin%5Cleft(z%5Cright)%260%5C%5C%20sin%5Cleft(z%5Cright)%26cos%5Cleft(z%5Cright)%260%5C%5C%200%260%261%5Cend%7Bpmatrix%7D?or=input
 
 namespace atkmath {
 
 Vector3 Matrix3::toEulerAnglesXYZ() const
 {
-   return Vector3();
+   
+   Matrix3 XYZ = *this;
+   
+   double theta = asin(XYZ.m13);
+   double beta;
+   double alpha;
+
+   if (theta == PI/2){
+      alpha = 0;
+   }
+   double beta = atan2(XYZ.m12 * -1, XYZ.m11);
+   double alpha = atan2(XYZ.m23 * -1, XYZ.m33);
+   return Vector3(alpha, theta, beta);
 }
 
 Vector3 Matrix3::toEulerAnglesXZY() const
 {
-   return Vector3();
+   Matrix3 XZY = *this;
+
+   double beta = asin(XZY.m12 * -1);
+   double theta = atan2(XZY.m13, XZY.m11);
+   double alpha = atan2(XZY.m32, XZY.m22);
+   return Vector3(alpha, theta, beta);
 }
 
 Vector3 Matrix3::toEulerAnglesYXZ() const
 {
-   return Vector3();
+   Matrix3 YXZ = *this;
+
+   double alpha = asin(YXZ.m23 * -1);
+   double beta = atan2(YXZ.m21, YXZ.m22);
+   double theta = atan2(YXZ.m13, YXZ.m33);
+   return Vector3(alpha, theta, beta);
 }
 
 Vector3 Matrix3::toEulerAnglesYZX() const
 {
-   return Vector3();
+   Matrix3 YZX = *this;
+
+   double beta = asin(YZX.m21);
+   double alpha = atan2(YZX.m23 * -1, YZX.m22);
+   double theta = atan2(YZX.m31 * -1, YZX.m11);
+   return Vector3(alpha, theta, beta);
 }
 
 Vector3 Matrix3::toEulerAnglesZXY() const
 {
-   return Vector3();
+   Matrix3 ZXY = *this;
+
+   double alpha = asin(ZXY.m32);
+   double theta = atan2(ZXY.m31 * -1, ZXY.m33);
+   double beta = atan2(ZXY.m12 * -1, ZXY.m22);
+   return Vector3(alpha, theta, beta);
 }
 
 Vector3 Matrix3::toEulerAnglesZYX() const
 {
-   return Vector3();
+   Matrix3 ZYX = *this;
+
+   double theta = asin(ZYX.m31*-1);
+   double beta;
+   double alpha;
+   if (theta == PI/2){
+      beta = 0;
+      alpha = atan2(ZYX.m12, ZYX.m13);
+   }else if (theta == (PI/2)*-1){
+      beta = 0;
+      alpha = atan2(ZYX.m12*-1, ZYX.m13*-1);
+   }else{
+      beta = atan2(ZYX.m21, ZYX.m11);
+      alpha = atan2(ZYX.m32, ZYX.m33);
+   }
+
+   return Vector3(alpha, theta, beta);
 }
 Matrix3 rotationX(float x){
    Matrix3 X;
@@ -73,92 +122,99 @@ Matrix3 rotationZ(float z){
 }
 void Matrix3::fromEulerAnglesXYZ(const Vector3& angleRad)
 {
-   *this = Identity;
+   //*this = Identity;
 
    float x = angleRad[0];
    float y = angleRad[1];
    float z = angleRad[2];
 
    Matrix3 X = rotationX(x);
-   Matrix3 Y = rotationX(y);
-   Matrix3 Z = rotationX(z);
+   Matrix3 Y = rotationY(y);
+   Matrix3 Z = rotationZ(z);
 
    Matrix3 XYZ = X*Y*Z;
+   *this = XYZ;
 }
 
 void Matrix3::fromEulerAnglesXZY(const Vector3& angleRad)
 {
-   *this = Identity;
+   //*this = Identity;
 
    float x = angleRad[0];
    float y = angleRad[1];
    float z = angleRad[2];
 
    Matrix3 X = rotationX(x);
-   Matrix3 Y = rotationX(y);
-   Matrix3 Z = rotationX(z);
+   Matrix3 Y = rotationY(y);
+   Matrix3 Z = rotationZ(z);
 
    Matrix3 XZY = X*Z*Y;
+   *this = XZY;
 }
 
 void Matrix3::fromEulerAnglesYXZ(const Vector3& angleRad)
 {
-   *this = Identity;
+   //*this = Identity;
 
    float x = angleRad[0];
    float y = angleRad[1];
    float z = angleRad[2];
 
    Matrix3 X = rotationX(x);
-   Matrix3 Y = rotationX(y);
-   Matrix3 Z = rotationX(z);
+   Matrix3 Y = rotationY(y);
+   Matrix3 Z = rotationZ(z);
 
    Matrix3 YXZ = Y*X*Z;
+   *this = YXZ;
+
 }
 
 void Matrix3::fromEulerAnglesYZX(const Vector3& angleRad)
 {
-   *this = Identity;
+   //*this = Identity;
 
    float x = angleRad[0];
    float y = angleRad[1];
    float z = angleRad[2];
 
    Matrix3 X = rotationX(x);
-   Matrix3 Y = rotationX(y);
-   Matrix3 Z = rotationX(z);
+   Matrix3 Y = rotationY(y);
+   Matrix3 Z = rotationZ(z);
 
    Matrix3 YZX = Y*Z*X;
+   *this = YZX;
 }
 
 void Matrix3::fromEulerAnglesZXY(const Vector3& angleRad)
 {
-   *this = Identity;
+   //*this = Identity;
 
    float x = angleRad[0];
    float y = angleRad[1];
    float z = angleRad[2];
 
    Matrix3 X = rotationX(x);
-   Matrix3 Y = rotationX(y);
-   Matrix3 Z = rotationX(z);
+   Matrix3 Y = rotationY(y);
+   Matrix3 Z = rotationZ(z);
 
    Matrix3 ZXY = Z*X*Y;
+   *this = ZXY;
 }
 
 void Matrix3::fromEulerAnglesZYX(const Vector3& angleRad)
 {
-   *this = Identity;
+   //*this = Identity;
 
    float x = angleRad[0];
    float y = angleRad[1];
    float z = angleRad[2];
 
    Matrix3 X = rotationX(x);
-   Matrix3 Y = rotationX(y);
-   Matrix3 Z = rotationX(z);
+   Matrix3 Y = rotationY(y);
+   Matrix3 Z = rotationZ(z);
 
    Matrix3 ZYX = Z*Y*X;
+   *this = ZYX;
 
 }
 
